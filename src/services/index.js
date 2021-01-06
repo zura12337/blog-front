09e78d4dd5a0fcf01e4b27d526a5898334e3bb22
zura12337 <zura.kurta@gmail.com>
2021-01-06 20:58:50 +0400

    ✨ Get Blogs from API.

---
+++ src/services/index.js
@@ -0,0 +1,13 @@
+import superagent from 'superagent';
+import superagentJsonapify from 'superagent-jsonapify';
+
+superagentJsonapify(superagent);
+
+export const getLatestBlogs = async (limit = '') => {
+  const response = await superagent.get(
+    `http://localhost/omedia-exercise/web/api/node/blog?sort=-created&page[limit]=${limit}&include=field_image,field_topic`
+  );
+  const body = response.body;
+  const data = body;
+  return data;
+};
