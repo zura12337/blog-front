0c0239219d16be680b541fd848b909b705205144
zura12337 <zura.kurta@gmail.com>
2021-01-07 16:42:02 +0400

    ✨ Topics Component.

---
+++ src/components/Topics.js
@@ -0,0 +1,42 @@
+import { Box, Link, Text } from '@chakra-ui/react';
+import React from 'react';
+
+export default function Topics({ topics }) {
+  return (
+    <Box
+      w={'100%'}
+      borderRadius={10}
+      px={'35px'}
+      py={15}
+      backgroundColor={'gray.100'}
+      h={200}
+    >
+      <Text textTransform="uppercase" letterSpacing={-0.5} fontSize={14}>
+        Discover More
+      </Text>
+      <Box>
+        {topics.map(topic => (
+          <Link
+            _hover={{
+              textDecoration: 'none',
+            }}
+            href={`/topic/${topic.name}`}
+            m="1.5px"
+            display="inline-block"
+            borderWidth="1px"
+            borderRadius={2}
+            backgroundColor="white"
+            borderColor={'black'}
+            p={1}
+            w={'max-content'}
+            h="max-content"
+            verticalAlign="center"
+            lineHeight="normal"
+          >
+            <Text fontSize={12}>{topic.name}</Text>
+          </Link>
+        ))}
+      </Box>
+    </Box>
+  );
+}
