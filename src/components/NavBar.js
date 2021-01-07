24458d645e16c6ec8155cf46680dc4a543729d6c
zura12337 <zura.kurta@gmail.com>
2021-01-07 21:11:27 +0400

    ✨ update navbar links.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -39,6 +39,18 @@ export default function NavBar() {
         </Link>
       </Heading>
       <Flex>
+        <Link
+          href="/blogs"
+          _hover={{ textDecoration: 'none' }}
+          _focus={{ outline: 'none' }}
+          px={10}
+          alignSelf="center"
+          fontSize={12}
+          fontFamily={'heading'}
+          fontWeight={600}
+        >
+          Blogs Listing
+        </Link>
         <Box py={2} px={10} bg={btnBg} justifySelf="flex-end" borderRadius={5}>
           <Text>
             <Link
